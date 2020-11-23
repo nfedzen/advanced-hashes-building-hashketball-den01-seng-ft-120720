0f7a59d450e8e61a39a931cc6b2e1a00312d3913
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-23 04:38:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,38 @@
 # Write your code here!
+def game_hash
+  hashketball = {
+    :home =>{
+      :team_name => " ",
+      :colors => [""],
+      :players => [
+        {:player_name => ""},
+        {:number => ""},
+        {:shoe => "" },
+        {:points => ""},
+        {:rebounds => ""},
+        {:assists => ""},
+        {:steals => ""},
+        {:blocks => ""},
+        {:slam_dunks => ""}
+      ]
+    },
+    :away => {
+      :team_name => " ",
+      :colors => [""],
+      :players => [
+        {:player_name => ""},
+        {:number => ""},
+        {:shoe => "" },
+        {:points => ""},
+        {:rebounds => ""},
+        {:assists => ""},
+        {:steals => ""},
+        {:blocks => ""},
+        {:slam_dunks => ""}
+      ]
+      
+    }
+    
+  }
+  
+end
