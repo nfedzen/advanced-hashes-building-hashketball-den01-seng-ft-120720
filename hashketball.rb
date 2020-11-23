9d116da78a8957f8dfd00ec757483ece9d420c8e
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-23 04:47:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
       :team_name => " ",
       :colors => [""],
       :players => [
-        :player_name => "Alan Anderson",
+        {:player_name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
         :points => 22,
@@ -13,7 +13,7 @@ def game_hash
         :assists => 12,
         :steals => 3,
         :blocks => 1,
-        :slam_dunks => 1
+        :slam_dunks => 1}
       ]
     },
     :away => {
